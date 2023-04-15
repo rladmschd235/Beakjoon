8c600f5acf64b72261a67c67905992b5c88eabb8
은총 <shhshsjsjd4@gmail.com>
2023-04-15 23:47:16 +0900

    2023-04-15

---
+++ Algorithm/Algorithm/문자열_11718.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str;
+
+	while (true)
+	{
+		getline(cin, str);
+
+		if (cin.eof())
+		{
+			break;
+		}
+		else
+		{
+			cout << str << endl;
+		}
+	}
+}
