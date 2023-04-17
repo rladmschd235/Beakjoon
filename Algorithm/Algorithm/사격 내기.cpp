f02e0de43447dd76ecd8c5f14317b33cf3690357
은총 <shhshsjsjd4@gmail.com>
2023-04-17 23:21:07 +0900

    2023-04-17

---
+++ Algorithm/Algorithm/사격 내기.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int ASum = 0;
+	int BSum = 0;
+}
---
+++ Algorithm/Algorithm/초코바.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+
+	if (N * 100 >= M)
+	{
+		cout << "Yes";
+	}
+	else
+	{
+		cout << "No";
+	}
+}
