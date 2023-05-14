31145d8c869dc859c0f1876d093868e8800f9f7a
은총 <shhshsjsjd4@gmail.com>
2023-05-14 23:37:34 +0900

    2023-05-14

---
+++ Algorithm/Algorithm/성택이의 은밀한 비밀번호_25372.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N; 
+	string str;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> str;
+
+		if (str.size() >= 6 && str.size() <= 9)
+		{
+			cout << "yes" << endl;
+		}
+		else
+		{
+			cout << "no" << endl;
+		}
+	}
+}
