d707c0dc2c65f4277201e9fd6da40eb5905496b2
은총 <shhshsjsjd4@gmail.com>
2023-05-28 21:34:25 +0900

    2023-05-28

--- Algorithm/Algorithm/A+B_26711cpp.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int A = 1997, B = 25, sum;
-    sum = A + B;
-    cout << sum;
-}
--- Algorithm/Algorithm/헛간 청약_19698.cpp
+++ Algorithm/Algorithm/헛간 청약_19698.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
- 
+
 int main()
 {
 	int N, W, H, L;
-	int divide;
-
+	int space, maxspace;
 	cin >> N >> W >> H >> L;
 
-	
+	space = (W / L) * (H / L);
+
+	if (space > N)
+	{
+		cout << N;
+		return 0;
+	}
+	cout << space;
 }
