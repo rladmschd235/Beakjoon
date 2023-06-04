9b1d265d94a968e29cd27d3082cb95fcc53ab458
은총 <shhshsjsjd4@gmail.com>
2023-06-04 23:55:53 +0900

    2023-06-04(2)

--- Algorithm/Algorithm/오각형, 오각형, 오각형…_1964.cpp
+++ Algorithm/Algorithm/오각형, 오각형, 오각형…_1964.cpp
@@ -3,9 +3,26 @@ using namespace std;
 
 int main()
 {
-	int N;
+	int N, count = 0, currentCount;
 	cin >> N;
 
-
-
+	for (int i = 0; i < N; i++)
+	{
+		if (i == 0)
+		{
+			count += 5;
+		}
+		else if (i == 1)
+		{
+			count += 7;
+			currentCount = 7;
+		}
+		else
+		{
+			count += currentCount + 3;
+			currentCount += 3;
+		}
+	}
+	
+	cout << count % 45678;
 }
