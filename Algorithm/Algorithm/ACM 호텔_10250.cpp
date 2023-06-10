dc4b53b1e00cbfb989508b68fe4071069efcfa1e
은총 <shhshsjsjd4@gmail.com>
2023-06-10 21:04:40 +0900

    2023-06-10

--- Algorithm/Algorithm/ACM 호텔_10250.cpp
+++ Algorithm/Algorithm/ACM 호텔_10250.cpp
@@ -6,29 +6,22 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr); cout.tie(nullptr);
 	
-	int H, W, N, T, Y, X;
+	int H, W, N, T, result;
 	cin >> T;
 
 	for (int i = 0; i < T; i++)
 	{
 		cin >> H >> W >> N;
-		Y = N;
-		X = 1;
-
-		while (true)
+		
+		if (N % H == 0)
 		{
-			if (Y > H)
-			{
-				Y -= H;
-				X++;
-			}
-			else if(Y < H)
-			{
-				break;
-			}
+			result = H * 100 + (N / H);
 		}
-
-		if(X < 10) cout << Y << '0' << X << "\n";
-		else cout << Y << X << "\n";
+		else
+		{
+			result = (N % H) * 100 + (N / H) + 1;
+		}
+		
+		cout << result << "\n";
 	}
 }
