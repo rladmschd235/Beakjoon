63efa799e73df3a5ee19ef2a21be97db76758a23
은총 <shhshsjsjd4@gmail.com>
2023-06-18 22:06:05 +0900

    2023-06-18

---
+++ Algorithm/Algorithm/숫자 카드_10815.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	map<int, int> cards = {};
+	int N, M, input;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> input;
+		cards[input]++;
+	}
+
+	cin >> M;
+
+	for (int i = 0; i < M; i++)
+	{
+		cin >> input;
+		cout << cards[input] << ' ';
+	}
+}
