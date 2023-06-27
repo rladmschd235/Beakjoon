f3144f8348e4e9bd5e736cae91a4c0e908e4088a
은총 <shhshsjsjd4@gmail.com>
2023-06-27 21:22:41 +0900

    2023-06-27

---
+++ Algorithm/Algorithm/인사성 밝은 곰곰이_25192.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	map<string, int> unm = {};
+	int N, cnt = 0;
+	string str;
+	
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> str;
+
+		if (str == "ENTER")
+		{
+			unm = {};
+			continue;
+		}
+		else
+		{
+			unm[str]++;
+		}
+
+		if (unm.find(str)->second == 1)
+		{
+			cnt++;
+		}
+		else
+		{
+			continue;
+		}
+	}
+
+	cout << cnt;
+}
