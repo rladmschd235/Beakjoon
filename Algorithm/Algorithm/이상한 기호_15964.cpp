502ec064d2c49387d0627d8ff7f841d184498b3c
은총 <shhshsjsjd4@gmail.com>
2023-06-30 22:41:49 +0900

    2023-06-30

---
+++ Algorithm/Algorithm/이상한 기호_15964.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long int a1, a2, answer1, answer2;
+
+	cin >> a1 >> a2;
+
+	answer1 = a1 + a2;
+	answer2 = a1 - a2;
+
+	cout << answer1 * answer2;
+}
