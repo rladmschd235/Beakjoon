1421a32c93220a707c793b2805833f52b46404e9
은총 <shhshsjsjd4@gmail.com>
2023-07-10 22:44:04 +0900

    2023-07-10

--- Algorithm/Algorithm/문자열 폭발_9935.cpp
+++ Algorithm/Algorithm/문자열 폭발_9935.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
 #include <string>
-#include <stack>
 using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    string str, bomb, answer, check;
+    int bombIndex;
+
+    cin >> str >> bomb;
+    for (int i = 0; i < str.size(); i++)
+    {
+        answer += str[i];
+        if (str[i] == bomb.back() && answer.size() >= bomb.size())
+        {
+            bombIndex = 0;
+            check = "";
+
+            for (int j = answer.size() - bomb.size(); j < answer.size(); j++)
+            {
+                if (bomb[bombIndex++] == answer[j])
+                {
+                    check += answer[j];
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (check == bomb)
+            {
+                answer.erase(answer.size() - bomb.size(), bomb.size());
+            }
+        }
+    }
+    if (answer == "")
+        cout << "FRULA";
+    else
+        cout << answer;
 }
---
+++ Algorithm/Algorithm/카약_2890.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+	char stadium[10][50];
+	int R, C;
+
+	for (int i = 0; i < R; i++)
+	{
+		for (int j = 0; j < C; j++)
+		{
+			cin >> stadium[i][j];
+		}
+	}
+}
