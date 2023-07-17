60822b4bd5aa6de7e0377f383cb0417a1a0248f0
은총 <shhshsjsjd4@gmail.com>
2023-07-17 20:32:23 +0900

    2023-07-17

---
+++ Algorithm/Algorithm/DNA 해독_1672.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	string DNA;
+
+	cin >> N >> DNA;
+}
--- Algorithm/Algorithm/패션왕 신해빈_9375.cpp
+++ Algorithm/Algorithm/패션왕 신해빈_9375.cpp
@@ -4,23 +4,30 @@ using namespace std;
 
 int main()
 {
-	int T, N;
-	string input1, input2;
+	string name, type;
+	int T, N, result = 1;
 
 	cin >> T;
 
 	for (int i = 0; i < T; i++)
 	{
+		// 종류 및 개수 입력 받을 맵과 결과를 초기화 해준다
+		unordered_map<string, int> clothesType; 
+		result = 1;
+
 		cin >> N;
 
 		for (int j = 0; j < N; j++)
 		{
-			unordered_map<string, string> unm;
-			cin >> input1 >> input2;
-
-			unm[input1] = input2;
-			
-			
+			cin >> name >> type;
+			clothesType[type]++; // 종류 별로 몇개의 옷이 있는지 세기
 		}
+		
+		for (auto clothes : clothesType)
+		{
+			result *= (clothes.second + 1); // 안 입었을 때 포함해서 종류별로 개수만큼 곱해주기(종류 별 개수 + 1(안입었을 때))
+		}
+
+		cout << result - 1 << endl; // 아무 것도 안입었을 때를 빼준다
 	}
-}
+} 
