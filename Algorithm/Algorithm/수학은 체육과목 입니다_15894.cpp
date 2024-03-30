6aef3ed5ab17ad1dda071302b37b100f8b3b46b6
은총 <shhshsjsjd4@gmail.com>
2024-03-30 12:36:37 +0900

    2024-03-29
    - 수학은 체육과목 입니다_15894
    - 최소공배수_13241

---
+++ Algorithm/Algorithm/수학은 체육과목 입니다_15894.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long int N;
+
+	cin >> N;
+
+	cout << N * 4;
+}
---
+++ Algorithm/Algorithm/최소공배수_13241.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+long long int GDC(long long int A, long long int B)
+{
+	if (B == 0)
+	{
+		return A;
+	}
+	return GDC(B, A % B);
+}
+
+int main()
+{
+	long long int result;
+	long long int A, B;
+
+	cin >> A >> B;
+
+	if (A < B)
+	{
+		int temp = A;
+		A = B;
+		B = temp;
+	}
+
+	result = (A * B) / GDC(A, B);
+
+	cout << result;
+}
