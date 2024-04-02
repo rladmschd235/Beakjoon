e0a53c6e0955adfe6dc9fed14fa88364f3feed57
은총 <shhshsjsjd4@gmail.com>
2024-04-02 09:39:31 +0900

    2024-04-02
    - 약수 구하기_2501

---
+++ Algorithm/Algorithm/약수 구하기_2501.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, K;
+
+	cin >> N >> K;
+
+	for (int i = 1; i < N; i++)
+	{
+		if (N % i == 0)
+		{
+			K--;
+		}
+		if (K == 0)
+		{
+			cout << i;
+			return 0;
+		}
+	}
+
+	cout << 0;
+}
