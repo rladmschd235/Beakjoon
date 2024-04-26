ff4c08cfe167d0fa57256621c01eb72ff366e16b
은총 <shhshsjsjd4@gmail.com>
2024-04-26 22:58:32 +0900

    2024-04-26
    - Fill the Rowboats!_5300

---
+++ Algorithm/Algorithm/Fill the Rowboats!_5300.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	for (int i = 1; i < n + 1; i++)
+	{
+		cout << i << " ";
+
+		if (i % 6 == 0)
+		{
+			cout << "Go! ";
+		}
+	}
+
+	cout << "Go! ";
+}
