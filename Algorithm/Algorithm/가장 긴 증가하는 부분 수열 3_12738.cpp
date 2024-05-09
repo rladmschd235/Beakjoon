ead8f90890db1d3009056ffe84195672db2d0710
은총 <shhshsjsjd4@gmail.com>
2024-05-09 20:19:07 +0900

    2024-05-08
    - 가장 긴 증가하는 부분 수열 3_12738

---
+++ Algorithm/Algorithm/가장 긴 증가하는 부분 수열 3_12738.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<long long> A;
+vector<long long> X;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int a;
+
+		cin >> a;
+		A.push_back(a);
+	}
+
+	X.push_back(A[0]);
+
+	for (int i = 1; i < N; i++)
+	{
+		if (X.back() < A[i])
+		{
+			X.push_back(A[i]);
+		}
+		else
+		{
+			int idx = lower_bound(X.begin(), X.end(), A[i]) - X.begin();
+			X[idx] = A[i];
+		}
+	}
+
+	cout << X.size();
+}
